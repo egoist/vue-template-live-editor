9706bc4e591c2489cee60a55e7365d299e45f452
EGOIST <0x142857@gmail.com>
2017-12-25 18:30:16 +0800

    offline support

--- poi.config.js
+++ poi.config.js
@@ -2,5 +2,12 @@ module.exports = {
   env: {
     VERSION: require('vue-template-compiler/package').version
   },
-  transformModules: ['vue-template-es2015-compiler']
+  transformModules: ['vue-template-es2015-compiler'],
+  presets: [
+    require('poi-preset-offline')({
+      pluginOptions: {
+        excludes: ['_redirects']
+      }
+    })
+  ]
 }
